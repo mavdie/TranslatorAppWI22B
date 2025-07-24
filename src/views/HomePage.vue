a7c8ca6f9f16e85a25d230f2229968f6d2c74249
mavdie <dietzmarvin@hotmail.de>
2025-07-24 15:44:10 +0200

    Added functions

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -51,6 +51,15 @@
             :icon="volumeHigh">
           </ion-icon>
         </ion-button>
+        <ion-button>
+          <ion-icon
+            id="copyToClipboard"
+            @click="copyToClipboard()"
+            slot="icon-only"
+            :icon="copy">
+          </ion-icon>
+        </ion-button>
+        <ion-toast trigger="copyToClipboard" message="Text copied to clipboard." :duration="5000"></ion-toast>
       </div>
     </ion-content>
   </ion-page>
@@ -66,9 +75,10 @@ import { IonContent,
   IonBackButton,
   IonItem,
   IonInput,
-  IonButton, } from '@ionic/vue';
+  IonButton,
+  IonToast, } from '@ionic/vue';
 import { defineComponent } from "vue";
-import { swapHorizontal, volumeHigh } from 'ionicons/icons';
+import { swapHorizontal, volumeHigh, copy } from 'ionicons/icons';
 import { Translation, Language } from '@capacitor-mlkit/translation';
 import { Clipboard } from '@capacitor/clipboard';
 import { SpeechSynthesis, AudioSessionCategory, QueueStrategy } from '@capawesome-team/capacitor-speech-synthesis';
@@ -84,10 +94,11 @@ export default defineComponent({
     IonBackButton,
     IonItem,
     IonInput,
+    IonToast,
     IonButton
   },
   setup() {
-    return {swapHorizontal, volumeHigh};
+    return {swapHorizontal, volumeHigh, copy};
   },
   data() {
     return {
@@ -109,16 +120,34 @@ export default defineComponent({
   },
   methods: {
     async translateText() {
-    const { text } = await Translation.translate({
+  this.disableButton = true
+  const { text } = await Translation.translate({
     text: this.sourceText,
     sourceLanguage: this.sourceLanguage,
-    targetLanguage: this.targetLanguage,
-  });
-  this.translation = text;
+    targetLanguage: this.targetLanguage
+  })
+  this.translation = text
+  this.disableButton = false
     },
     async swapLanguages() {
+      // Swap the selected languages
+      const temp = this.sourceLanguage;
+      this.sourceLanguage = this.targetLanguage;
+      this.targetLanguage = temp;
+      // Swap the text in the textareas as well
+      const tempText = this.sourceText;
+      this.sourceText = this.translation;
+      this.translation = tempText;
     },
     async readAloud() {
+      await SpeechSynthesis.speak({
+        text: this.translation
+      });
+    },
+    async copyToClipboard() {
+      await Clipboard.write({
+        string: this.translation
+      });
     }
   },
 
@@ -128,7 +157,7 @@ export default defineComponent({
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -144,9 +173,9 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
